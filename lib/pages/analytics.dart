6c2a88e44db63015c44212e096ff8abb3294f215
Musa-Husseini <74230559+Musa-Husseini@users.noreply.github.com>
2022-11-08 17:02:39 -0800

    Added navbar to each page, made a change to the appbar.dart, made it a switch case statement, and depending on the title of the page it will show certain routes to other pages

--- lib/pages/analytics.dart
+++ lib/pages/analytics.dart
@@ -1,14 +1,11 @@
 // ignore_for_file: file_names, prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import '../main_widgets/appbar.dart';
 
 class AnalyticsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('Analytics'),
-          centerTitle: true,
-          backgroundColor: Colors.black,
-        ),
+        appBar: HeaderNav(context, 'Analytics'),
       );
 }
