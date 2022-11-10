2a0311a868cd805fcff74bd3b908e8e830788fc6
Tom Arad <tom.arad@wsu.edu>
2022-11-10 15:14:19 -0800

    changed color scheme

--- lib/pallete.dart
+++ lib/pallete.dart
@@ -1,23 +1,39 @@
-// ignore_for_file: unnecessary_const, duplicate_ignore
+// ignore_for_file: unnecessary_const, duplicate_ignore, prefer_const_constructors
 
 //palette.dart
 import 'package:flutter/material.dart';
 
+const Map<int, Color> colorCrim = {
+  50: Color.fromRGBO(166, 15, 45, .1),
+  100: Color.fromRGBO(166, 15, 45, .2),
+  200: Color.fromRGBO(166, 15, 45, .3),
+  300: Color.fromRGBO(166, 15, 45, .4),
+  400: Color.fromRGBO(166, 15, 45, .5),
+  500: Color.fromRGBO(166, 15, 45, .6),
+  600: Color.fromRGBO(166, 15, 45, .7),
+  700: Color.fromRGBO(166, 15, 45, .8),
+  800: Color.fromRGBO(166, 15, 45, .9),
+  900: Color.fromRGBO(166, 15, 45, 1),
+};
+
+const Map<int, Color> colorGray = {
+  50: Color.fromRGBO(177, 77, 77, .1),
+  100: Color.fromRGBO(177, 77, 77, .2),
+  200: Color.fromRGBO(177, 77, 77, .3),
+  300: Color.fromRGBO(177, 77, 77, .4),
+  400: Color.fromRGBO(177, 77, 77, .5),
+  500: Color.fromRGBO(177, 77, 77, .6),
+  600: Color.fromRGBO(177, 77, 77, .7),
+  700: Color.fromRGBO(177, 77, 77, .8),
+  800: Color.fromRGBO(177, 77, 77, .9),
+  900: Color.fromRGBO(177, 77, 77, 1),
+};
+
 class Palette {
   // ignore: unnecessary_const
-  static const MaterialColor ktoCrimson = const MaterialColor(
-    0xffDC143C, // 0% comes in here, this will be color picked if no shade is selected when defining a Color property which doesn’t require a swatch.
-    const <int, Color>{
-      50: const Color(0xffce5641), //10%
-      100: const Color(0xffb74c3a), //20%
-      200: const Color(0xffa04332), //30%
-      300: const Color(0xff89392b), //40%
-      400: const Color(0xff733024), //50%
-      500: const Color(0xff5c261d), //60%
-      600: const Color(0xff451c16), //70%
-      700: const Color(0xff2e130e), //80%
-      800: const Color(0xff170907), //90%
-      900: const Color(0xff000000), //100%
-    },
-  );
-} // you can define define int 500 as the default shade and add your lighter tints above and darker tints below.
+  static const MaterialColor ktoCrimson =
+      const MaterialColor(0xFFA60F2D, colorCrim);
+
+  static const MaterialColor ktoGray =
+      const MaterialColor(0xFF4D4D4D, colorGray);
+}
