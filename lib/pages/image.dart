2cf2f1dd7aa7f84b68009bb9b7179a6e6658687d
Tom Arad <tom.arad@wsu.edu>
2023-04-11 13:30:30 -0700

    removed download button from images

--- lib/pages/image.dart
+++ lib/pages/image.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:image_downloader_web/image_downloader_web.dart';
 
 class ImageDialog extends StatelessWidget {
   final String imageUrl;
 
   const ImageDialog({required this.imageUrl});
-
-  Future<void> _downloadImage(BuildContext context) async {
-    await WebImageDownloader.downloadImageFromWeb(imageUrl, imageQuality: 100);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,13 +17,6 @@ class ImageDialog extends StatelessWidget {
             children: [
               const SizedBox(height: 16),
               Image.network(imageUrl),
-              const SizedBox(height: 16),
-              TextButton(
-                onPressed: () {
-                  _downloadImage(context);
-                },
-                child: const Text('Download'),
-              ),
               const SizedBox(height: 16),
               TextButton(
                 onPressed: () => Navigator.of(context).pop(),
