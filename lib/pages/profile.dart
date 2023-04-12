cc5f78e5224d507183a67bdf6e3dc92fd129a1a8
Nathan Bunge <nathan.bunge@wsu.edu>
2023-04-11 22:19:48 -0700

    added email validator to profile page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -213,6 +213,10 @@ class _ProfilePageState extends State<ProfilePage> {
                     if (value == null || value.isEmpty) {
                       return 'Please enter your email address';
                     }
+                    if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                        .hasMatch(value)) {
+                      return 'Please enter a valid email address';
+                    }
                     return null;
                   },
                   onSaved: (value) {
