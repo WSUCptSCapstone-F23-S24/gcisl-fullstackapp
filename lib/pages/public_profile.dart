100114b2dd1aefc7561053c5cb7481f826df5b15
Anthony Salvione <anthony.salvione@wsu.edu>
2023-11-03 15:31:07 -0700

    Update public_profile.dart
    Now shows a list of user's posts when you go to view their profile or your own profile

--- lib/pages/public_profile.dart
+++ lib/pages/public_profile.dart
@@ -5,10 +5,19 @@ import 'dart:html';
 import 'package:csc_picker/csc_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:geocode/geocode.dart';
 import 'package:geocoding/geocoding.dart';
+
+import 'dart:math';
+import 'dart:async';
+import 'package:uuid/uuid.dart';
+
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'image.dart';
 
 import '../palette.dart';
 
@@ -79,11 +88,9 @@ class _ProfilePage1State extends State<ProfilePage1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: ListView(
         children: [
-          const Expanded(flex: 1, child: _TopPortion()),
-          Expanded(
-            flex: 4,
+          Container(
             child: Padding(
               padding: const EdgeInsets.all(6.0),
               child: Column(
@@ -113,7 +120,8 @@ class _ProfilePage1State extends State<ProfilePage1> {
                     ],
                   ),
                   const SizedBox(height: 16),
-                  _ProfileInfoRow(_companyPositionController, _companyController, _countryAddressController, _phoneController)
+                  _ProfileInfoRow(_companyPositionController, _companyController, _countryAddressController, _phoneController),
+                  PostPortion()
                 ],
               ),
             ),
@@ -257,5 +265,283 @@ class _TopPortion extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class PostPortion extends StatefulWidget {
+  @override
+  State<PostPortion> createState() => _PostPortionState();
+}
+  enum PostSortOption { newest, oldest, alphabetical }
+
+class _PostPortionState extends State<PostPortion> {
+  PostSortOption? _selectedSortOption = PostSortOption.newest;
+  final List _postList = [];
+  String? emailHash;
+  String? username;
+  String? currentEmail;
+  int _displayedPosts = 30;
+
+   final DatabaseReference _database =
+      FirebaseDatabase.instance.ref().child('posts');
+
+  void _sortPostList()
+  {
+    print("sorting\n");
+      switch (_selectedSortOption) {
+        case PostSortOption.newest:
+          _postList.sort((a, b) => b[2].compareTo(a[2])); 
+          break;
+        case PostSortOption.oldest:
+          _postList.sort((a, b) => a[2].compareTo(b[2])); 
+          break;
+        case PostSortOption.alphabetical:
+          _postList.sort((a, b) => (a[0] as String).compareTo(b[0] as String)); 
+          break;
+    }
+    setState(() {}); 
+  }
+
+  Future<String?> getCurrentUser() async {
+    String? name;
+    await FirebaseDatabase.instance
+        .ref('users')
+        .get()
+        // ignore: avoid_function_literals_in_foreach_calls
+        .then((snapshot) => snapshot.children.forEach((element) {
+              if (element.key.toString() == emailHash) {
+                name = element.child("first name").value.toString() +
+                    " " +
+                    element.child("last name").value.toString();
+              }
+            }));
+    return name;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    emailHash = FirebaseAuth.instance.currentUser?.email?.hashCode.toString();
+    getCurrentUser().then((value) {
+      setState(() {
+        username = value;
+      });
+    });
+    currentEmail = FirebaseAuth.instance.currentUser?.email;
+    _database.onChildAdded.listen(_onNewPostAdded);
+  }
+
+  void _onNewPostAdded(DatabaseEvent event) {
+    String? uniquePostId = event.snapshot.key;
+    String? uniquePostImageId = event.snapshot.child("image").key;
+    final newPost = event.snapshot.child("text").value.toString();
+    String? userName = event.snapshot.child("user_name").value.toString();
+    String? timestamp = event.snapshot.child("timestamp").value.toString();
+    String? image = event.snapshot.child("image").value.toString();
+    String? email = event.snapshot.child("email").value.toString();
+    if (userName == "null") {
+      userName = "anonymous";
+    }
+    if (mounted && email == currentEmail) {
+      setState(() {
+        _postList.insert(0, [newPost, userName, timestamp, image,email,uniquePostId,uniquePostImageId]);
+        _sortPostList();
+      });
+    }
+  }
+
+    @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 16),
+              DropdownButton<PostSortOption>(
+                    value: _selectedSortOption,
+                    onChanged: (newSortOption) {
+                      setState(() {
+                        _selectedSortOption = newSortOption;
+                        // Sort the post list based on the selected option
+                        _sortPostList();
+                      });
+                    },
+                    items: [
+                      DropdownMenuItem(
+                        value: PostSortOption.newest,
+                        child: Text('Most Recent'),
+                      ),
+                      DropdownMenuItem(
+                        value: PostSortOption.oldest,
+                        child: Text('Oldest'),
+                      ),
+                      DropdownMenuItem(
+                        value: PostSortOption.alphabetical,
+                        child: Text('Alphabetical (A-Z)'),
+                      ),
+                    ],
+                  ),
+              const SizedBox(height: 16),
+              if (_postList.isNotEmpty)
+                Column(children: [
+                  Container(
+                    width: 900,
+                    margin: const EdgeInsets.symmetric(horizontal: 16),
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: min(_postList.length, _displayedPosts),
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 15,
+                            ),
+                            child: Column(children: [
+                              Card(
+                                child: Column(children: [
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  TextButton(
+                                    child: Text(
+                                      _postList[index][1] ?? "anonymous",
+                                      style: const TextStyle(
+                                        decoration: TextDecoration.underline,
+                                        color: Palette.ktoCrimson,
+                                        fontWeight: FontWeight.w600,
+                                      fontSize: 16,
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(builder: (context) => ProfilePage1(_postList[index][4].hashCode.toString()))
+                                      );
+                                      //ProfilePage1(_postList[index][4].hashCode.toString());
+                                    }
+                                  ),
+                                  _postList[index][0] == ""
+                                      ? Container(
+                                          constraints: const BoxConstraints(
+                                              minHeight: 75),
+                                        )
+                                      : Column(
+                                          children: [
+                                            const SizedBox(
+                                              height: 8,
+                                            ),
+                                            Container(
+                                              constraints: const BoxConstraints(
+                                                  minHeight: 75),
+                                              child: Column(
+                                                children: [
+                                                  ListTile(
+                                                    title: SelectableText(
+                                                      _postList[index][0],
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                  _postList[index][3] == "null"
+                                      ? const SizedBox(height: 0)
+                                      : MouseRegion(
+                                          cursor: SystemMouseCursors.click,
+                                          child: GestureDetector(
+                                            onTap: () {
+                                              showDialog(
+                                                context: context,
+                                                builder: (context) =>
+                                                    ImageDialog(
+                                                        imageUrl:
+                                                            _postList[index]
+                                                                [3]),
+                                              );
+                                            },
+                                            child: Column(
+                                              children: [
+                                                //const SizedBox(height: 2),
+                                                SizedBox(
+                                                  child: Image.network(
+                                                    _postList[index][3],
+                                                    fit: BoxFit.scaleDown,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                  const SizedBox(height: 8),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                      left: 16,
+                                    ),
+                                    child: SelectableText(
+                                      DateFormat('MM/dd/yyyy hh:mm a').format(
+                                          DateTime.fromMillisecondsSinceEpoch(
+                                              int.parse(_postList[index][2]))),
+                                      style: const TextStyle(
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ),
+                                  
+                                ]),
+                              ),
+                            ]));
+                      },
+                    ),
+                    
+                  ),
+                  if (_postList.length > _displayedPosts)
+                    SizedBox(
+                      width: 125,
+                      height: 65,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: Card(
+                          color: Palette.ktoCrimson,
+                          child: TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _displayedPosts += 30;
+                              });
+                            },
+                            child: const Text(
+                              'Load More',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 19),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                ]),
+              if (_postList.isEmpty)
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    'No posts yet.',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              const SizedBox(height: 16),
+              const Text(
+                'You Have Reached the End \u{1F60A}',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
   }
 }
