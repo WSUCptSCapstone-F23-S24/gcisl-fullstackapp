5bd4cf263598f2597cc2b5fabd7ec372e085778c
Anthony Salvione <89549595+AnthBoss@users.noreply.github.com>
2024-02-27 15:41:28 -0800

    Revert "Feature comment preview"

--- lib/pages/CommentsPage.dart
+++ lib/pages/CommentsPage.dart
@@ -1,13 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:gcisl_app/main.dart';
 import 'package:gcisl_app/pages/home.dart';
 import 'package:gcisl_app/palette.dart';
 import 'package:google_maps_webservice/directions.dart';
 import 'package:intl/intl.dart';
-import 'package:like_button/like_button.dart';
 
 class CommentsPage extends StatefulWidget {
   final String postId;
@@ -69,13 +67,6 @@ class _CommentsPageState extends State<CommentsPage> {
         String sender = event.snapshot.child('sender').value.toString();
         String timeStampString =
             event.snapshot.child('timestamp').value.toString();
-        var likes = [];
-        if(event.snapshot.child('likes').value == null) {
-          likes = [];
-        } else {
-          likes = event.snapshot.child('likes').value as List;
-        }
-        //var likes = event.snapshot.child('likes').value as List;
         DateTime timestamp = DateTime.parse(timeStampString);
         final formattedTimeStamp =
             DateFormat('MM/dd/yyyy hh:mm a').format(timestamp);
@@ -86,10 +77,8 @@ class _CommentsPageState extends State<CommentsPage> {
             commentID: commentID,
             commentText: text,
             commentedBy: sender,
-            commentLikes: likes,
             commentTime: formattedTimeStamp,
             replies: loadReply));
-
       });
     });
   }
@@ -136,7 +125,6 @@ class _CommentsPageState extends State<CommentsPage> {
     _commentRef.child(commentID).set({
       // Generates a unique comment ID
       'text': text,
-      'likes': [],
       'sender': emailHash.toString(),
       'timestamp': timestamp,
       'replies': [],
@@ -196,14 +184,6 @@ class _CommentsPageState extends State<CommentsPage> {
     _replyController.clear();
   }
 
-    Future<void> _updateLikesInDatabase(String commentID, String postID, List likes) async {
-      final postRef = _database
-        .child(postID)
-        .child('comments')
-        .child(commentID);
-      await postRef.child('likes').set(likes);
-    }
-
   @override
   Widget build(BuildContext context) {
     print("entered build");
@@ -233,86 +213,27 @@ class _CommentsPageState extends State<CommentsPage> {
                     ": " +
                     _comments[index].commentText);
                 final comment = _comments[index];
-                final likes = _comments[index].commentLikes;
                 int commentIndex = index;
                 if (comment != null) {
-                  return Card(
-                    elevation: 4, // Add elevation for a box-like appearance
-                    margin: EdgeInsets.all(
-                        16), // Add margin for spacing between cards
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        width: 2,
-                        color: Colors.grey,
-                      ),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                comment.commentedBy,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                comment.commentTime.toString() ?? '',
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              Text(comment.commentText ?? ''),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          child: Row(
-                            children: [
-                              LikeButton(
-                                likeCount: likes.length,
-                                //countPostion: CountPostion.bottom,
-                                isLiked: likes.contains(widget.username),
-
-                                onTap: (isLiked) async {
-                                  setState(() {
-                                    if (isLiked) {
-                                      likes.remove(widget.username);
-                                    } else {
-                                      likes.add(widget.username);
-                                    }
-                                  });
-                                  await _updateLikesInDatabase(
-                                      _comments[index].commentID, widget.postId, likes);
-                                  return Future.value(!isLiked);
-                                },
-                                likeBuilder: (isLiked) {
-                                  return Icon(
-                                    Icons.thumb_up_sharp,
-                                    color: isLiked
-                                        ? Colors.blue
-                                        : Colors.grey,
-                                  );
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: TextButton(
-                                onPressed: () {
-                                  // When reply button is pressed it pops up a dialog box for the user to write a comment.
-                                  _openDialog(_comments[index].commentID);
-                                },
-                                
-                                child: Text('Reply'),
+                  return FutureBuilder(
+                      future: _getUserName(comment.commentedBy),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return CircularProgressIndicator(); // Or any other loading indicator
+                        } else if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          final username = snapshot.data ?? '';
+                          return Card(
+                            elevation:
+                                4, // Add elevation for a box-like appearance
+                            margin: EdgeInsets.all(
+                                16), // Add margin for spacing between cards
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                width: 2,
+                                color: Colors.grey,
                               ),
                               borderRadius: BorderRadius.circular(8),
                             ),
@@ -337,35 +258,20 @@ class _CommentsPageState extends State<CommentsPage> {
                                           color: Colors.grey,
                                         ),
                                       ),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.all(8),
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  reply.repliedBy,
-                                                  style: TextStyle(
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
-                                                ),
-                                                Text(
-                                                  reply.replyTime.toString() ??
-                                                      '',
-                                                  style: TextStyle(
-                                                    color: Colors.grey,
-                                                  ),
-                                                ),
-                                                Text(reply.replyText ?? ''),
-                                              ],
-                                              
-                                            ),
-                                          ),
-                                        ],
+                                      Text(comment.commentText ?? ''),
+                                    ],
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      child: TextButton(
+                                        onPressed: () {
+                                          // When reply button is pressed it pops up a dialog box for the user to write a comment.
+                                          _openDialog(
+                                              _comments[index].commentID);
+                                        },
+                                        child: Text('Reply'),
                                       ),
                                     ),
                                   ],
@@ -470,12 +376,9 @@ class _CommentsPageState extends State<CommentsPage> {
                                 ),
                               ],
                             ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    
-                  );
+                          );
+                        }
+                      });
                 } else {
                   return SizedBox();
                 }
@@ -521,7 +424,6 @@ class Comment {
   final String commentID;
   final String commentText;
   final String commentedBy;
-  final List commentLikes;
   final String commentTime;
   final List<Reply> replies;
 
@@ -529,7 +431,6 @@ class Comment {
     required this.commentID,
     required this.commentText,
     required this.commentedBy,
-    required this.commentLikes,
     required this.commentTime,
     required this.replies,
   });
