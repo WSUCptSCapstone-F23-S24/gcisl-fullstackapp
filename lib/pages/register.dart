31d66b8d8f690b9267cecf4f08bb61e189029fdb
joshua-frey-wsu <joshua.frey@wsu.edu>
2024-03-31 16:11:38 -0700

    Added Required Field Symbol to Location Section of Profile and Registration Page

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -347,103 +347,118 @@ class _RegisterPageState extends State<RegisterPage> {
                 const SizedBox(height: 30.0),
                 Container(
                   width: MediaQuery.of(context).size.width * 0.50,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: CSCPicker(
-                    ///Enable disable state dropdown [OPTIONAL PARAMETER]
-                    showStates: true,
-
-                    /// Enable disable city drop down [OPTIONAL PARAMETER]
-                    showCities: true,
-
-                    ///Enable (get flag with country name) / Disable (Disable flag) / ShowInDropdownOnly (display flag in dropdown only) [OPTIONAL PARAMETER]
-                    flagState: CountryFlag.DISABLE,
-
-                    ///Dropdown box decoration to style your dropdown selector [OPTIONAL PARAMETER] (USE with disabledDropdownDecoration)
-                    dropdownDecoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                        color: Colors.white,
-                        border:
-                            Border.all(color: Colors.grey.shade300, width: 1)),
-
-                    ///Disabled Dropdown box decoration to style your dropdown selector [OPTIONAL PARAMETER]  (USE with disabled dropdownDecoration)
-                    disabledDropdownDecoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                        color: Colors.grey.shade300,
-                        border:
-                            Border.all(color: Colors.grey.shade300, width: 1)),
-
-                    ///placeholders for dropdown search field
-                    countrySearchPlaceholder: "Country",
-                    stateSearchPlaceholder: "State",
-                    citySearchPlaceholder: "City",
-
-                    ///labels for dropdown
-                    countryDropdownLabel: "Country",
-                    stateDropdownLabel: "State",
-                    cityDropdownLabel: "City",
-
-                    ///Default Country
-                    defaultCountry: CscCountry.United_States,
-
-                    ///Disable country dropdown (Note: use it with default country)
-                    //disableCountry: true,
-
-                    ///selected item style [OPTIONAL PARAMETER]
-                    selectedItemStyle: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
+                  child: InputDecorator(
+                    decoration: InputDecoration(
+                      label: RichText(
+                        text: const TextSpan(
+                            text: 'Location',
+                            style: const TextStyle(color: Colors.black),
+                            children: [
+                              TextSpan(
+                                  text: ' *',
+                                  style: TextStyle(
+                                    color: Colors.red,
+                                  ))
+                            ]),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: BorderSide.none,
+                      ),
                     ),
+                    child: CSCPicker(
+                      ///Enable disable state dropdown [OPTIONAL PARAMETER]
+                      showStates: true,
 
-                    ///DropdownDialog Heading style [OPTIONAL PARAMETER]
-                    dropdownHeadingStyle: const TextStyle(
+                      /// Enable disable city drop down [OPTIONAL PARAMETER]
+                      showCities: true,
+
+                      ///Enable (get flag with country name) / Disable (Disable flag) / ShowInDropdownOnly (display flag in dropdown only) [OPTIONAL PARAMETER]
+                      flagState: CountryFlag.DISABLE,
+
+                      ///Dropdown box decoration to style your dropdown selector [OPTIONAL PARAMETER] (USE with disabledDropdownDecoration)
+                      dropdownDecoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                          color: Colors.white,
+                          border: Border.all(
+                              color: Colors.grey.shade300, width: 1)),
+
+                      ///Disabled Dropdown box decoration to style your dropdown selector [OPTIONAL PARAMETER]  (USE with disabled dropdownDecoration)
+                      disabledDropdownDecoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                          color: Colors.grey.shade300,
+                          border: Border.all(
+                              color: Colors.grey.shade300, width: 1)),
+
+                      ///placeholders for dropdown search field
+                      countrySearchPlaceholder: "Country",
+                      stateSearchPlaceholder: "State",
+                      citySearchPlaceholder: "City",
+
+                      ///labels for dropdown
+                      countryDropdownLabel: "Country",
+                      stateDropdownLabel: "State",
+                      cityDropdownLabel: "City",
+
+                      ///Default Country
+                      defaultCountry: CscCountry.United_States,
+
+                      ///Disable country dropdown (Note: use it with default country)
+                      //disableCountry: true,
+
+                      ///selected item style [OPTIONAL PARAMETER]
+                      selectedItemStyle: const TextStyle(
                         color: Colors.black,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold),
+                        fontSize: 14,
+                      ),
 
-                    ///DropdownDialog Item style [OPTIONAL PARAMETER]
-                    dropdownItemStyle: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
+                      ///DropdownDialog Heading style [OPTIONAL PARAMETER]
+                      dropdownHeadingStyle: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+
+                      ///DropdownDialog Item style [OPTIONAL PARAMETER]
+                      dropdownItemStyle: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                      ),
+
+                      ///Dialog box radius [OPTIONAL PARAMETER]
+                      dropdownDialogRadius: 10.0,
+
+                      ///Search bar radius [OPTIONAL PARAMETER]
+                      searchBarRadius: 10.0,
+
+                      ///triggers once country selected in dropdown
+                      onCountryChanged: (value) {
+                        setState(() {
+                          ///store value in country variable
+                          countryValue = value;
+                        });
+                      },
+
+                      ///triggers once state selected in dropdown
+                      onStateChanged: (value) {
+                        setState(() {
+                          ///store value in state variable
+                          stateValue = value;
+                        });
+                      },
+
+                      ///triggers once city selected in dropdown
+                      onCityChanged: (value) {
+                        setState(() {
+                          ///store value in city variable
+                          cityValue = value;
+                        });
+                      },
+
+                      ///Show only specific countries using country filter
+                      // countryFilter: ["United States", "Canada", "Mexico"],
                     ),
-
-                    ///Dialog box radius [OPTIONAL PARAMETER]
-                    dropdownDialogRadius: 10.0,
-
-                    ///Search bar radius [OPTIONAL PARAMETER]
-                    searchBarRadius: 10.0,
-
-                    ///triggers once country selected in dropdown
-                    onCountryChanged: (value) {
-                      setState(() {
-                        ///store value in country variable
-                        countryValue = value;
-                      });
-                    },
-
-                    ///triggers once state selected in dropdown
-                    onStateChanged: (value) {
-                      setState(() {
-                        ///store value in state variable
-                        stateValue = value;
-                      });
-                    },
-
-                    ///triggers once city selected in dropdown
-                    onCityChanged: (value) {
-                      setState(() {
-                        ///store value in city variable
-                        cityValue = value;
-                      });
-                    },
-
-                    ///Show only specific countries using country filter
-                    // countryFilter: ["United States", "Canada", "Mexico"],
                   ),
                 ),
                 const SizedBox(height: 20.0),
